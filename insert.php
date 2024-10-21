fb1013de0a1e9267cc3cd98ee621149d7050e066
crmllaaaa <103046184+crmllaaaa@users.noreply.github.com>
2024-10-22 03:01:25 +0800

    Create insert.php

---
+++ insert.php
@@ -0,0 +1,30 @@
+<?php require_once 'core/dbConfg.php'; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hospital Medicine Inventory Management System</title>
+</head>
+<body>
+    <pre>
+    <?php
+    // insert new user
+    $username = 'crmllaaaa';
+    $password = password_hash('yanto123', PASSWORD_DEFAULT); // Hash the password
+    $role = 'staff';
+
+    //  insert statement
+    $stmt = $pdo->prepare("INSERT INTO users (username, password, role) VALUES (?, ?, ?)");
+    
+    if ($stmt->execute([$username, $password, $role])) {
+        echo "Insert Record successfully.";
+    } else {
+        echo "Insert Record failed.";
+    }
+    ?>
+    </pre>
+</body>
+</html>
+
