1699dc467c2188c80d0fe4dd3ad92c1cbebd2eec
crmllaaaa <103046184+crmllaaaa@users.noreply.github.com>
2024-10-22 03:01:54 +0800

    Create delete.php

---
+++ delete.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hospital Medicine Inventory Management System</title>
+</head>
+<body>
+<pre>
+    <?php
+    // ID of the user to delete
+    $userId = 1; 
+
+    //  delete statement
+    $stmt = $pdo->prepare("DELETE FROM users WHERE id = ?");
+    
+    if ($stmt->execute([$userId])) {
+        echo "Record deleted successfully.";
+    } else {
+        echo "Deleting record failed.";
+    }
+    ?>
+    </pre>
+</body>
+</html>
