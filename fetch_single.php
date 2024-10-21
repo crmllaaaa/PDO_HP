095882232f8b15d84ed2c32a6b095ff10574afb2
crmllaaaa <103046184+crmllaaaa@users.noreply.github.com>
2024-10-22 03:00:22 +0800

    Create fetch_single.php

---
+++ fetch_single.php
@@ -0,0 +1,23 @@
+<?php require_once 'core/dbConfg.php'; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hospital Medicine Inventory Management System</title>
+</head>
+<body>
+    <pre>
+    <?php
+    // Select one row from the tabe medicines
+    $stmt = $pdo->prepare("SELECT * FROM medicines LIMIT 1");
+    if ($stmt->execute()) {
+        // Fetch single row
+        $row = $stmt->fetch();
+        print_r($row);
+    }
+    ?>
+    </pre>
+</body>
+</html>
